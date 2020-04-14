ac3a92401140b92da3e7a580168b6e5a99f51aa3
rvanderaspoilden <rodolphevanderaspoilden@gmail.com>
2020-04-14 16:35:41 +0100

    :sparkles: build mode in progress

---
+++ Assets/Scripts/Building/BuildPreview.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Sim.Building {
+    public class BuildPreview : MonoBehaviour {
+        [Header("Settings")]
+        [SerializeField] private Material errorMaterial;
+
+        [Header("Only for debug")]
+        [SerializeField] private Renderer[] renderers;
+        [SerializeField] private Material[] defaultRendererMaterials;
+        [SerializeField] private NavMeshObstacle navMeshObstacle;
+        [SerializeField] private bool placeable;
+
+        [SerializeField] private List<Collider> colliderTriggered;
+
+        private void Awake() {
+            this.colliderTriggered = new List<Collider>();
+            this.renderers = GetComponentsInChildren<Renderer>();
+            this.defaultRendererMaterials = this.renderers.ToList().Select(x => x.material).ToArray();
+            this.navMeshObstacle = GetComponentInChildren<NavMeshObstacle>();
+
+            if (navMeshObstacle) { // disable this to avoid collision with player agent
+                navMeshObstacle.enabled = false;
+            }
+        }
+
+        private void OnTriggerStay(Collider other) {
+            if (other.gameObject.layer != LayerMask.NameToLayer("Ground") && !this.colliderTriggered.Find(x => x == other)) {
+                this.colliderTriggered.Add(other);
+            }
+            
+            this.CheckValidity();
+        }
+
+        private void OnTriggerExit(Collider other) {
+            this.colliderTriggered.Remove(other);
+            this.CheckValidity();
+        }
+
+        private void CheckValidity() {
+            this.placeable = this.colliderTriggered.Count == 0;
+
+            if (this.placeable) {
+                for (int i = 0; i < this.defaultRendererMaterials.Length; i++) {
+                    this.renderers[i].material = this.defaultRendererMaterials[i];
+                }
+            } else {
+                foreach (Renderer renderer in this.renderers) {
+                    renderer.material = this.errorMaterial;
+                }
+            }
+        }
+
+        public void SetErrorMaterial(Material mat) {
+            this.errorMaterial = mat;
+        }
+
+        public bool IsPlaceable() {
+            return this.placeable;
+        }
+    }   
+}
--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Cinemachine;
+using Photon.Pun;
 using Photon.Pun.Demo.PunBasics;
+using Sim.Building;
+using Sim.Enums;
 using Sim.Interactables;
 using UnityEngine;
 
@@ -8,9 +11,27 @@ namespace Sim {
     public class CameraManager : MonoBehaviour {
         [Header("Settings")]
         [SerializeField] private CinemachineFreeLook freelookCamera;
-        
+
         [Header("Only for debug")]
         [SerializeField] private new Camera camera;
+
+        [SerializeField] private CameraModeEnum currentMode;
+
+        [Header("Build settings")]
+        [SerializeField] private float cameraRotationSpeed = 3f;
+
+        [SerializeField] private float cameraMoveSpeed = 3f;
+        [SerializeField] private float cameraZoomSpeed = 3f;
+
+        [SerializeField] private float propsRotationSpeed = 1.5f;
+        [SerializeField] private GameObject propsToInstantiate;
+
+        [SerializeField] private Material errorMaterial;
+
+        [Header("Build debug")]
+        [SerializeField] private GameObject currentPropSelected;
+
+        [SerializeField] private BuildPreview currentPreview;
 
         private RaycastHit hit;
 
@@ -27,35 +48,108 @@ namespace Sim {
         }
 
         private void Start() {
-            this.freelookCamera.m_XAxis.m_MaxSpeed = 0f;
+            this.SwitchToFreeMode();
         }
 
         void Update() {
-            this.ManageInteraction();
+            if (this.currentMode == CameraModeEnum.FREE) {
+                this.ManageInteraction();
+            } else if (this.currentMode == CameraModeEnum.BUILD) {
+                this.ManageBuildMode();
+            }
 
             if (Input.GetMouseButtonDown(1)) {
                 this.freelookCamera.m_XAxis.m_MaxSpeed = 300f;
             }
-            
+
             if (Input.GetMouseButtonUp(1)) {
                 this.freelookCamera.m_XAxis.m_MaxSpeed = 0f;
+            }
+
+
+            // todo to remove
+            if (Input.GetKeyDown(KeyCode.B)) {
+                this.SwitchToBuildMode();
+            } else if (Input.GetKeyDown(KeyCode.F)) {
+                this.SwitchToFreeMode();
+            }
+        }
+
+        public void SwitchToBuildMode() {
+            this.currentMode = CameraModeEnum.BUILD;
+            this.freelookCamera.enabled = false;
+        }
+
+        public void SwitchToFreeMode() {
+            this.currentMode = CameraModeEnum.FREE;
+            this.freelookCamera.enabled = true;
+            this.freelookCamera.m_XAxis.m_MaxSpeed = 0f;
+        }
+
+        private void ManageBuildMode() {
+            // Manage camera movement
+            if (Input.GetMouseButton(1)) { // Rotation
+                this.transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * this.cameraRotationSpeed);
+                this.transform.eulerAngles = new Vector3(this.transform.rotation.eulerAngles.x, this.transform.rotation.eulerAngles.y, 0);
+            }
+
+            if (Input.GetMouseButton(2)) { // Movement
+                this.transform.Translate(new Vector3(-Input.GetAxis("Mouse X"), -Input.GetAxis("Mouse Y"), 0) * this.cameraMoveSpeed);
+            }
+
+            float mouseScrollValue = Input.GetAxis("Mouse ScrollWheel");
+
+            if (mouseScrollValue != 0f) { // Zoom
+                this.transform.Translate(Vector3.forward * mouseScrollValue * this.cameraZoomSpeed);
+            }
+
+            // todo move that later
+            if (Input.GetKeyDown(KeyCode.A) && !this.currentPropSelected) {
+                this.currentPropSelected = Instantiate(this.propsToInstantiate);
+                this.currentPropSelected.layer = LayerMask.NameToLayer("Preview");
+                this.currentPreview = this.currentPropSelected.AddComponent<BuildPreview>();
+                this.currentPreview.SetErrorMaterial(this.errorMaterial);
+            }
+
+            // Manage detection on ground and props
+            int layerMask = this.currentPropSelected ? (1 << 9) : (1 << 9 | 1 << 10);
+            
+            if (Physics.Raycast(this.camera.ScreenPointToRay(Input.mousePosition), out hit, 100, layerMask)) {
+                if (this.currentPropSelected) {
+                    if (Input.GetKey(KeyCode.LeftArrow)) {
+                        this.currentPropSelected.transform.Rotate(Vector3.forward * -this.propsRotationSpeed);
+                    } else if (Input.GetKey(KeyCode.RightArrow)) {
+                        this.currentPropSelected.transform.Rotate(Vector3.forward * this.propsRotationSpeed);
+                    }
+
+                    if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground")) {
+                        this.currentPropSelected.transform.position = hit.point;
+                    }
+
+                    if (Input.GetMouseButtonDown(0) && this.currentPreview.IsPlaceable()) {
+                        PhotonNetwork.Instantiate("Prefabs/" + this.propsToInstantiate.name, this.currentPropSelected.transform.position, this.currentPropSelected.transform.rotation);
+                        Destroy(this.currentPropSelected);
+                    }
+                } else {
+                    Debug.Log(hit.collider.name);
+                }
             }
         }
 
         private void ManageInteraction() {
             if (Physics.Raycast(this.camera.ScreenPointToRay(Input.mousePosition), out hit)) {
                 Interactable objectToInteract = null;
-                
+
                 if (hit.collider.CompareTag("Interactable")) {
                     Interactable interactable = hit.collider.GetComponentInParent<Interactable>();
-                    
+
                     // todo display cursor for interaction
 
                     if (interactable.CanInteractWithPlayer(RoomManager.LocalPlayer)) {
                         objectToInteract = interactable;
                     }
                 }
-                
+
                 if (Input.GetMouseButtonDown(0)) {
                     if (objectToInteract) {
                         objectToInteract.Interact();
