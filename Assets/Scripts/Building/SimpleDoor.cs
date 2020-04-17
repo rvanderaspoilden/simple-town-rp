48e87c3c5ae3a064dc0955076ffe7ae3d6589767
rvanderaspoilden <rodolphevanderaspoilden@gmail.com>
2020-04-17 12:46:49 +0100

    :sparkles: add door system

---
+++ Assets/Scripts/Building/SimpleDoor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sim.Building {
+    public class SimpleDoor : MonoBehaviour {
+        [Header("Only for debug")]
+        [SerializeField] private Animator animator;
+        [SerializeField] private List<Collider> colliderTriggered;
+
+        private void Awake() {
+            this.colliderTriggered = new List<Collider>();
+            this.animator = GetComponent<Animator>();
+        }
+
+        private void OnTriggerStay(Collider other) {
+            if (other.gameObject.layer == LayerMask.NameToLayer("Player") && !this.colliderTriggered.Find(x => x == other)) {
+                this.colliderTriggered.Add(other);
+            }
+            
+            this.UpdateAnimator();
+        }
+
+        private void OnTriggerExit(Collider other) {
+            this.colliderTriggered.Remove(other);
+            this.UpdateAnimator();
+        }
+
+        private void UpdateAnimator() {
+            this.animator.SetBool("isOpened", this.colliderTriggered.Count > 0);
+        }
+    }
+}
+
