ad1f9399a80cd3dc097c2d0a5121612c3f31c2d5
Rodolphe <rodolphevanderaspoilden@outlook.fr>
2020-11-17 21:18:18 +0100

    :recycle: add phone controller UI

--- Assets/Scripts/UI/DefaultViewUI.cs
+++ Assets/Scripts/UI/DefaultViewUI.cs
@@ -17,17 +17,6 @@ namespace Sim {
         [SerializeField]
         private RectTransform phone;
 
-        private bool phoneOpened;
-        
-        private Vector2 firstPressPos;
-        private Vector2 secondPressPos;
-        private Vector2 currentSwipe;
-        private float defaultPhoneAnchorPosY;
-
-        private void Awake() {
-            this.defaultPhoneAnchorPosY = phone.anchoredPosition.y;
-        }
-
         private void Start() {
             this.characterInfoPanelUI.Setup(NetworkManager.Instance.Personnage);
             this.SetLocationText(PhotonNetwork.CurrentRoom.Name);
@@ -36,51 +25,6 @@ namespace Sim {
         private void SetLocationText(string value) {
             foreach (TextMeshProUGUI tmpPro in this.locationText.GetComponentsInChildren<TextMeshProUGUI>()) {
                 tmpPro.text = value;
-            }
-        }
-
-        private void Update() {
-            this.Swipe();
-        }
-
-        public void OpenPhone() {
-            if (!this.phoneOpened) {
-                this.phone.DOAnchorPosY(0, 0.5f).SetEase(Ease.Flash).OnComplete(() => this.phoneOpened = true);
-            }
-        }
-
-        public void ClosePhone() {
-            if (this.phoneOpened) {
-                this.phone.DOAnchorPosY(this.defaultPhoneAnchorPosY, 1).SetEase(Ease.OutBounce).OnComplete(() => this.phoneOpened = false);
-            }
-        }
-        
-        // TODO: it's temporary
-        public void Swipe()
-        {
-            if(Input.GetMouseButtonDown(0))
-            {
-                firstPressPos = new Vector2(Input.mousePosition.x,Input.mousePosition.y);
-            }
-            if(Input.GetMouseButtonUp(0))
-            {
-                secondPressPos = new Vector2(Input.mousePosition.x,Input.mousePosition.y);
-       
-                currentSwipe = new Vector2(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
-           
-                currentSwipe.Normalize();
- 
-                //swipe upwards
-                if(currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
-                {
-                    this.OpenPhone();
-                }
-                
-                //swipe down
-                if(currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
-                {
-                    this.ClosePhone();
-                }
             }
         }
     }
---
+++ Assets/Scripts/UI/PhoneControllerUI.cs
@@ -0,0 +1,79 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Sim {
+    public class PhoneControllerUI : MonoBehaviour, IPointerDownHandler {
+        [Header("Settings")]
+        [SerializeField]
+        private float openedAnchorY;
+
+        [SerializeField]
+        private float openAnimationDuration;
+        
+        [SerializeField]
+        private float closeAnimationDuration;
+
+        private Vector2 firstPressPos;
+        private Vector2 secondPressPos;
+        private Vector2 currentSwipe;
+        
+        private bool phoneOpened;
+        
+        private float defaultPhoneAnchorPosY;
+
+        private RectTransform rectTransform;
+        
+        private void Awake() {
+            this.rectTransform = GetComponent<RectTransform>();
+            this.defaultPhoneAnchorPosY = this.rectTransform.anchoredPosition.y;
+            this.firstPressPos = Vector2.negativeInfinity;
+        }
+
+        private void Update() {
+            this.Swipe();
+        }
+
+        public void OnPointerDown(PointerEventData eventData) {
+            firstPressPos = new Vector2(Input.mousePosition.x,Input.mousePosition.y);
+        }
+        
+        public void Swipe()
+        {
+            if(Input.GetMouseButtonUp(0) && this.firstPressPos != Vector2.negativeInfinity)
+            {
+                secondPressPos = new Vector2(Input.mousePosition.x,Input.mousePosition.y);
+       
+                currentSwipe = new Vector2(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+           
+                currentSwipe.Normalize();
+ 
+                //swipe upwards
+                if(currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+                {
+                    this.OpenPhone();
+                }
+                
+                //swipe down
+                if(currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+                {
+                    this.ClosePhone();
+                }
+
+                this.firstPressPos = Vector2.negativeInfinity;
+            }
+        }
+        
+        private void OpenPhone() {
+            if (!this.phoneOpened) {
+                this.rectTransform.DOAnchorPosY(this.openedAnchorY, this.openAnimationDuration).SetEase(Ease.Flash).OnComplete(() => this.phoneOpened = true);
+            }
+        }
+
+        private void ClosePhone() {
+            if (this.phoneOpened) {
+                this.rectTransform.DOAnchorPosY(this.defaultPhoneAnchorPosY, this.closeAnimationDuration).SetEase(Ease.OutBounce).OnComplete(() => this.phoneOpened = false);
+            }
+        }
+    }
+}
