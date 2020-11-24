aea1cff6119502a97a12c2df7365805e5b808d22
Rodolphe <rodolphevanderaspoilden@outlook.fr>
2020-11-24 22:23:30 +0100

    :sparkles: add custom cursors

---
+++ Assets/Scripts/Managers/CursorManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Sim {
+    public class CursorManager : MonoBehaviour {
+        [Header("Settings")]
+        [SerializeField]
+        private Texture2D buildCursor;
+        
+        [Header("Debug")]
+        [SerializeField]
+        private Texture2D currentCursor;
+
+        public static CursorManager Instance;
+
+        private void Awake() {
+            if (Instance != null) {
+                Destroy(this.gameObject);
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        public void SetCursor(Texture2D cursorTexture) {
+            this.currentCursor = cursorTexture;
+            Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
+        }
+
+        public void SetBuildCursor() {
+            this.SetCursor(this.buildCursor);
+        }
+
+        public Texture2D GetCursor() {
+            return this.currentCursor;
+        }
+    }
+}
---
+++ Assets/Scripts/Scriptables/CursorConfig.cs
@@ -0,0 +1,13 @@
+﻿using Sim.Enums;
+using UnityEngine;
+
+namespace Sim.Scriptables {
+    [CreateAssetMenu(fileName = "Cursor", menuName = "Configurations/Cursor")]
+    public class CursorConfig : ScriptableObject {
+        [SerializeField]
+        private Texture2D texture2D;
+
+        [SerializeField]
+        private CursorTypeEnum type;
+    }
+}
