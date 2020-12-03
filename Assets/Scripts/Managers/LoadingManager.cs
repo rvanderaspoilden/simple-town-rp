766d62ad8797dc1102269dcc7aa0c7dacffdbd27
Rodolphe <rodolphevanderaspoilden@outlook.fr>
2020-12-03 23:03:26 +0100

    :lipstick: improve loading screen

--- Assets/Scripts/Managers/LoadingManager.cs
+++ Assets/Scripts/Managers/LoadingManager.cs
@@ -4,17 +4,29 @@ using UnityEngine.UI;
 
 namespace Sim {
     public class LoadingManager : MonoBehaviour {
-
         [Header("Settings")]
         [SerializeField]
         [Min(0)]
         private float fadeInDuration;
-        
+
         [SerializeField]
         [Min(0)]
         private float fadeOutDuration;
-        
-        private Image image;
+
+        [SerializeField]
+        private float maxLoaderOpacity;
+
+        [SerializeField]
+        private float maxFrontOpacity;
+
+        [SerializeField]
+        private Image backgroundImg;
+
+        [SerializeField]
+        private Image frontImg;
+
+        [SerializeField]
+        private Image loaderImg;
 
         public delegate void StateChanged(bool isActive);
 
@@ -27,20 +39,25 @@ namespace Sim {
                 Destroy(this.gameObject);
             }
 
-            this.image = GetComponentInChildren<Image>();
-            this.image.color = new Color(1, 1, 1, 0);
+            this.frontImg.color = new Color(1, 1, 1, 0);
+            this.backgroundImg.color = new Color(1, 1, 1, 0);
+            this.loaderImg.color = new Color(1, 1, 1, 0);
 
             Instance = this;
             DontDestroyOnLoad(this.gameObject);
         }
 
-        public void Show() {
-            this.image.DOColor(new Color(1, 1, 1, 1), this.fadeInDuration);
+        public void Show(bool instant = false) {
+            this.backgroundImg.DOColor(new Color(1, 1, 1, 1), instant ? 0 : this.fadeInDuration);
+            this.frontImg.DOColor(new Color(1, 1, 1, this.maxFrontOpacity), instant ? 0 : this.fadeInDuration);
+            this.loaderImg.DOColor(new Color(1, 1, 1, this.maxLoaderOpacity), instant ? 0 : this.fadeInDuration);
             OnStateChanged?.Invoke(true);
         }
 
         public void Hide() {
-            this.image.DOColor(new Color(1, 1, 1, 0), this.fadeOutDuration).OnComplete(() => OnStateChanged?.Invoke(false));
+            this.backgroundImg.DOColor(new Color(1, 1, 1, 0), this.fadeOutDuration).OnComplete(() => OnStateChanged?.Invoke(false));
+            this.frontImg.DOColor(new Color(1, 1, 1, 0), this.fadeOutDuration);
+            this.loaderImg.DOColor(new Color(1, 1, 1, 0), this.fadeOutDuration);
         }
     }
 }
