d2df58d58362b8cee4a23041d2f69e3f93bd11de
Rodolphe <rodolphe@jetdev.fr>
2022-11-28 14:02:56 +0100

    :sparkles: change game configuration

--- Assets/Scripts/Managers/MeteoManager.cs
+++ Assets/Scripts/Managers/MeteoManager.cs
@@ -5,7 +5,8 @@ public class MeteoManager : MonoBehaviour {
     [SerializeField]
     private PolyverseSkies _polyverseSkies;
 
-    private readonly float HOUR_CONSTANT = 1 / 12f;
+    private readonly float POLYVERSE_HOUR_CONSTANT = 1 / 12f;
+    private readonly float ROTATION_HOUR_CONSTANT = 360 / 24f;
 
     private void FixedUpdate() {
         int hours = int.Parse(TimeManager.CurrentTime.ToString(@"hh"));
@@ -13,11 +14,17 @@ public class MeteoManager : MonoBehaviour {
         float value = 0;
 
         if (hours < 12) {
-            value = 1 - (hours * HOUR_CONSTANT);
+            value = 1 - (hours * POLYVERSE_HOUR_CONSTANT);
         } else if (hours > 12) {
-            value = (hours - 12) * HOUR_CONSTANT;
+            value = (hours - 12) * POLYVERSE_HOUR_CONSTANT;
         }
 
         this._polyverseSkies.timeOfDay = value;
+        
+        this.ManageRotation(hours);
+    }
+
+    private void ManageRotation(int hours) {
+        this.transform.rotation = Quaternion.Euler((hours * ROTATION_HOUR_CONSTANT) + 180f,0,0);
     }
 }
